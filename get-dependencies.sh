6f3881e8ef36e072dd0fa0483dc7cb96e31d552e
CodingPuppy <codingpapi@gmail.com>
2026-03-26 16:15:01 +0800

    Update get-dependencies.sh

--- get-dependencies.sh
+++ get-dependencies.sh
@@ -1,24 +1,17 @@
 #!/bin/sh
-
 set -eu
-
 ARCH=$(uname -m)
-
 echo "Installing package dependencies..."
 echo "---------------------------------------------------------------"
-pacman -Syu --noconfirm patchelf nodejs npm bun
-
+pacman -Syu --noconfirm patchelf curl nodejs npm
 echo "Installing debloated packages..."
 echo "---------------------------------------------------------------"
 get-debloated-pkgs --add-common --prefer-nano
-
-# Comment this out if you need an AUR package
 make-aur-package openspec
 
-# openspec 已装好，找入口文件并编译成单文件二进制
-# 用 tr 去掉换行符
-echo "Entry point: $(pacman -Ql openspec | grep 'dist/cli/index.js' | grep -v '\.map' | awk '{print $2}' | tr -d '\n')"
+# 用 pkg 编译成单文件二进制
+npm install -g @yao-pkg/pkg
 ENTRY=$(pacman -Ql openspec | grep 'dist/cli/index.js' | grep -v '\.map' | awk '{print $2}' | tr -d '\n')
-bun build "$ENTRY" --compile --outfile /usr/bin/openspec-standalone
-
-# If the application needs to be manually built that has to be done down here
+# 先确认 openspec 用的 node 版本
+NODE_VER=$(node --version | cut -d'v' -f2 | cut -d'.' -f1)
+pkg "$ENTRY" --targets "node${NODE_VER}-linux-${ARCH}" --output /usr/bin/openspec-standalone
