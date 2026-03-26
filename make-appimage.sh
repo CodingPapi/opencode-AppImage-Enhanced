dfb58931d14d714011ecf9fdb732bfd462accbc1
CodingPuppy <codingpapi@gmail.com>
2026-03-26 17:36:58 +0800

    Update make-appimage.sh

--- make-appimage.sh
+++ make-appimage.sh
@@ -4,7 +4,6 @@ ARCH=$(uname -m)
 VERSION=$(pacman -Q kiro-cli | awk '{print $2; exit}')
 export ARCH VERSION
 export OUTPATH=./dist
-export ADD_HOOKS="self-updater.bg.hook"
 export UPINFO="gh-releases-zsync|${GITHUB_REPOSITORY%/*}|${GITHUB_REPOSITORY#*/}|latest|*$ARCH.AppImage.zsync"
 
 # kiro-cli 没有图标和桌面文件，手动创建
