624b5060648fa681ce353f7457b23ac28b760707
CodingPuppy <codingpapi@gmail.com>
2026-03-26 15:45:12 +0800

    Update get-dependencies.sh

--- get-dependencies.sh
+++ get-dependencies.sh
@@ -6,7 +6,7 @@ ARCH=$(uname -m)
 
 echo "Installing package dependencies..."
 echo "---------------------------------------------------------------"
-pacman -Syu --noconfirm patchelf nodejs npm
+pacman -Syu --noconfirm patchelf nodejs npm bun
 
 echo "Installing debloated packages..."
 echo "---------------------------------------------------------------"
@@ -14,5 +14,9 @@ get-debloated-pkgs --add-common --prefer-nano
 
 # Comment this out if you need an AUR package
 make-aur-package openspec
+
+# openspec 已装好，找入口文件并编译成单文件二进制
+ENTRY=$(pacman -Ql openspec | grep 'dist/cli/index.js' | awk '{print $2}')
+bun build "$ENTRY" --compile --outfile /usr/bin/openspec-standalone
 
 # If the application needs to be manually built that has to be done down here
