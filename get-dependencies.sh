e31665a72fc99ddf909a7ed81937ff755951486d
CodingPuppy <codingpapi@gmail.com>
2026-03-26 15:32:58 +0800

    Update get-dependencies.sh

--- get-dependencies.sh
+++ get-dependencies.sh
@@ -6,13 +6,13 @@ ARCH=$(uname -m)
 
 echo "Installing package dependencies..."
 echo "---------------------------------------------------------------"
-pacman -Syu --noconfirm patchelf gst-plugins-good gst-plugins-bad
+pacman -Syu --noconfirm patchelf curl imagemagick
 
 echo "Installing debloated packages..."
 echo "---------------------------------------------------------------"
 get-debloated-pkgs --add-common --prefer-nano
 
 # Comment this out if you need an AUR package
-make-aur-package opencode-bin
+make-aur-package kiro-cli
 
 # If the application needs to be manually built that has to be done down here
