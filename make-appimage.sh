5da93256943c9aa2790718301fbf053885cfd042
CodingPuppy <codingpapi@gmail.com>
2026-03-26 17:12:42 +0800

    Update make-appimage.sh

--- make-appimage.sh
+++ make-appimage.sh
@@ -18,7 +18,7 @@ export ICON=./AppDir/usr/share/icons/hicolor/128x128/apps/openspec.png
 cat <<EOF > ./AppDir/usr/share/applications/openspec.desktop
 [Desktop Entry]
 Name=OpenSpec
-Exec=openspec
+Exec=openspec-run
 Icon=openspec
 Type=Application
 Categories=Development;
@@ -29,9 +29,9 @@ export DESKTOP=./AppDir/usr/share/applications/openspec.desktop
 export DEPLOY_OPENGL=0
 export DEPLOY_P11KIT=0
 
-# 同时部署 node 运行时和 openspec 脚本
+# 部署 wrapper 脚本和 node 运行时
 quick-sharun \
-    /usr/bin/openspec \
+    /usr/bin/openspec-run \
     /usr/bin/node
 
 # 把 openspec 的 lib 目录（包含 node_modules）复制进 AppDir
