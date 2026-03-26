ec50bbfa1bb52a0ad5be118b378936bdc6a6bb73
CodingPuppy <codingpapi@gmail.com>
2026-03-26 17:37:17 +0800

    Update make-appimage.sh

--- make-appimage.sh
+++ make-appimage.sh
@@ -4,7 +4,6 @@ ARCH=$(uname -m)
 VERSION=$(pacman -Q openspec | awk '{print $2; exit}')
 export ARCH VERSION
 export OUTPATH=./dist
-export ADD_HOOKS="self-updater.bg.hook"
 export UPINFO="gh-releases-zsync|${GITHUB_REPOSITORY%/*}|${GITHUB_REPOSITORY#*/}|latest|*$ARCH.AppImage.zsync"
 
 mkdir -p ./AppDir/usr/share/icons/hicolor/128x128/apps
